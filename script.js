c244efe7e6ef74810cf2e1278aa6908d9a62a11e
rosa479 <sahrohan183@gmail.com>
2025-01-13 21:54:59 +0530

    updates

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const Btn = document.getElementById("submitBtn");
 const wttr = document.getElementById("desc");
 const temp = document.getElementById("temp");
+const sun = document.getElementById("sun");
+const wind = document.getElementById("wind");
+const cityDate = document.getElementById("cityDate");
+const humidity = document.getElementById("humidity");
+const pressure = document.getElementById("pressure");
 console.log(wttr.textContent)
 const apiKey = "f79536a5964bfd8f52914bbd049cfea1";
 city=`chennai`;
@@ -19,8 +24,15 @@ Btn.addEventListener("click", event => {
         const dataObject = checkWeather(city);
         const Update = async () => {
             const data = await dataObject;
+            cityDate.textContent=`${data.name}, ${data.sys.country} | ${new Date().toDateString()}`;
             temp.textContent=`${Math.round(data.main.temp)}°C` ;
             wttr.textContent=`${data.weather[0].main}`;
+            const sunrise = new Date(data.sys.sunrise*1000);
+            const sunset = new Date(data.sys.sunset*1000);
+            wind.textContent=`Wind Speed: ${data.wind.speed} m/s`;
+            sun.textContent=`Sunrise: ${sunrise.getHours()}:${sunrise.getMinutes()} AM, Sunset: ${sunset.getHours()}:${sunset.getMinutes()} PM`;
+            humidity.textContent=`Humidity: ${data.main.humidity}%`;
+            pressure.textContent=`Pressure: ${data.main.pressure} hPa`;
             console.log(data);
           };
         Update()
